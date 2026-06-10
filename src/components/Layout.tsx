4af67f24b490f1811365532135ef32e3ee5b092e
khmatin06 <khalilovmetin2006@gmail.com>
2026-06-11 01:17:22 +0400

    Update Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,11 +5,8 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-<<<<<<< HEAD
-//Page layout with page content and navbar
-=======
+
 // Page layout with page content and navbar 
->>>>>>> 6e04683 (update project)
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="pitch-bg min-h-screen">
